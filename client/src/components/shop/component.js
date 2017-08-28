ae3ae5a8dbae51d9d3608cc4379ecee785f51336
Jonathan Rim <jonrim@umich.edu>
2017-08-28 16:07:32 -0400

    .

--- client/src/components/shop/component.js
+++ client/src/components/shop/component.js
@@ -245,7 +245,7 @@ export default class Shop extends Component {
                           <Button icon='remove' color='red' size='tiny' onClick={e => this.removeFromCart(index)}/>
                         </Table.Cell>
                         <Table.Cell>
-                          <Image shape='rounded' src={item.info.imageUrls[0]} />
+                          <Image shape='rounded' src={item.info.imageUrls.split(',')[0]} />
                         </Table.Cell>
                         <Table.Cell>
                           {item.info.name}
